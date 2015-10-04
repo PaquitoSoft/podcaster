cbe93275a080365fe6a4b5e53dd2a85e7161e505
PaquitoSoft <paquitosoftware@gmail.com>
2015-10-04 19:50:35 +0200

    Fix on podcast favorite updating ui

--- app/js/components/partials/podcast-summary.js
+++ app/js/components/partials/podcast-summary.js
@@ -6,18 +6,10 @@
 	var PodcastSummary = Ractive.extend({
 		isolated: true,
 		template: templates.getTemplate('podcast-summary-partial-tpl'),
-		oninit() {
-			// console.log('Podcast:', this.get());
+		oninit: function() {
 			this.on('toggleFavorite', function(event) {
-				event.original.preventDefault();
-				event.original.stopPropagation();
-				var podcast = this.get().podcast;
-				// this.get('podcast').toggleFavorite();
-				// this.get().podcast.toggleFavorite();
-				// this.update('podcast');
-				// this.update();
-				// this.set('podcast', this.get().podcast);
-				this.set('podcast', podcast);
+				this.get('podcast').toggleFavorite();
+				this.set('podcast', this.get('podcast'));
 				return false;
 			});
 		}
